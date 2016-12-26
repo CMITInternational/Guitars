c0a808db51aa43ac01acb2f2ea75c8c882d0939f
darcy-buttrose <darcy.buttrose@gmail.com>
2016-12-26 16:23:17 +0800

    added admin password as LetMeIn backwards and base64

--- Web/src/containers/App/Reducer.js
+++ Web/src/containers/App/Reducer.js
@@ -9,14 +9,16 @@ const ACTION_HANDLERS = {
   }),
   [Keys.LOAD_APP_CONFIG]: (state: Object, action: Action): Object => Object.assign({}, state, {
     apiUrl: action.payload.apiUrl,
-    assetUrl: action.payload.assetUrl
+    assetUrl: action.payload.assetUrl,
+    admin: action.payload.admin
   })
 };
 
 export const initialState = {
   isReady: false,
   apiUrl: 'api/',
-  assetUrl: ''
+  assetUrl: '',
+  admin: ''
 };
 
 export default function appReducer (state: object = initialState, action: Action): object {
