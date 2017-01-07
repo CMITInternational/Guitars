a7731635656024c0c2b385d111dd2a978f877dde
darcy-buttrose <darcy.buttrose@gmail.com>
2017-01-07 10:48:34 +0800

    added rotating items to home page

--- Web/src/containers/Home/index.js
+++ Web/src/containers/Home/index.js
@@ -5,12 +5,9 @@ import { button } from 'react-bootstrap';
 import appActions from '../App/Actions';
 import type IApp from '../App/IApp';
 import appPropTypes from '../App/PropTypes';
-import Center from 'react-center';
 
 type IProps = {
   app: IApp,
-  containerWidth: number,
-  containerHeight: number,
   showHeader: Function,
   redirectToProducts: Function
 }
@@ -18,8 +15,6 @@ type IProps = {
 class Home extends React.Component<void, IProps, void> {
   static propTypes = {
     app: React.PropTypes.shape(appPropTypes).isRequired,
-    containerWidth: React.PropTypes.number.isRequired,
-    containerHeight: React.PropTypes.number.isRequired,
     showHeader: React.PropTypes.func.isRequired,
     redirectToProducts: React.PropTypes.func.isRequired
   };
@@ -28,9 +23,9 @@ class Home extends React.Component<void, IProps, void> {
     super(props);
 
     this.state = {
+      processId: undefined,
       backImage: {
-        index: undefined,
-        processId: undefined,
+        index: 0,
         collection: [
           'assets/DSC_0141.jpg',
           'assets/DSC_0143.jpg',
@@ -40,10 +35,38 @@ class Home extends React.Component<void, IProps, void> {
           'assets/DSC_0149.jpg',
           'assets/DSC_0150.jpg'
         ]
+      },
+      frontText: {
+        index: 0,
+        collection: [
+          'Phillip J Buttrose Pty Ltd',
+          'Phillip Buttrose - Luthier',
+          '0411 228 482',
+          'phillip.buttrose@iinet.net.au'
+        ]
       }
     };
 
     this.setBackImage = this.setBackImage.bind(this);
+    this.setFrontText = this.setFrontText.bind(this);
+    this.swapContent = this.swapContent.bind(this);
+  }
+
+  swapContent () {
+    this.setBackImage();
+    this.setFrontText();
+  }
+
+  setFrontText () {
+    let newFrontTextIndex = 0;
+    if (this.state.frontText.index !== undefined && this.state.frontText.index !== this.state.frontText.collection.length - 1) {
+      newFrontTextIndex = this.state.frontText.index + 1;
+    }
+    this.setState({
+      frontText: Object.assign({}, this.state.frontText, {
+        index: newFrontTextIndex
+      })
+    });
   }
 
   setBackImage () {
@@ -60,13 +83,11 @@ class Home extends React.Component<void, IProps, void> {
 
   componentWillMount () {
     this.props.showHeader(false);
-    this.setBackImage();
-    if (this.state.backImage.processId === undefined) {
-      let setBackImageProcId = setInterval(this.setBackImage, 3000);
+    this.swapContent();
+    if (this.state.processId === undefined) {
+      let setBackImageProcId = setInterval(this.swapContent, 3000);
       this.setState({
-        backImage: Object.assign({}, this.state.backImage, {
-          processId: setBackImageProcId
-        })
+        processId: setBackImageProcId
       });
     }
   }
@@ -75,12 +96,10 @@ class Home extends React.Component<void, IProps, void> {
   }
 
   componentWillUnmount () {
-    if (this.state.backImage.processId !== undefined) {
+    if (this.state.processId !== undefined) {
       clearInterval(this.state.backImage.processId);
       this.setState({
-        backImage: Object.assign({}, this.state.backImage, {
-          processId: undefined
-        })
+        processId: undefined
       });
     }
   }
@@ -90,14 +109,20 @@ class Home extends React.Component<void, IProps, void> {
       height: '100%',
       backgroundImage: `url(${this.props.app.assetUrl}${this.state.backImage.collection[this.state.backImage.index]})`,
       backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat'
+      backgroundRepeat: 'no-repeat',
+      alignContent: 'center',
+      alignItems: 'center',
+      boxSizing: 'border-box',
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'wrap',
+      justifyContent: 'center'
     };
 
     return (
       <div style={divStyle}>
-        <Center style={{height: '100%'}}>
-          <button style={{background: 'none', border: 'none'}} onClick={this.props.redirectToProducts}>Enter</button>
-        </Center>
+        <h1>{this.state.frontText.collection[this.state.frontText.index]}</h1>
+        <button style={{background: 'none', border: 'none'}} onClick={this.props.redirectToProducts}>Click Here To Enter</button>
       </div>
     );
   }
